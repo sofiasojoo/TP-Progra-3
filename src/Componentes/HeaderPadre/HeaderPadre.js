e14da67959ab4aba16e8f51244cb6ec77509e76e
sofiasojoo <ssojo@udesa.edu.ar>
2025-09-09 18:11:15 -0300

    header cambio

--- src/Componentes/HeaderPadre/HeaderPadre.js
+++ src/Componentes/HeaderPadre/HeaderPadre.js
@@ -5,11 +5,11 @@ import { Link } from "react-router-dom";
 function HeaderPadre(){
     let itemsMenu=[{ruta:"/", name:"Home"},{ruta:"/favoritos", name:"Favoritos"}, {ruta:"/populares", name:"Populares"}, {ruta:"/cartel", name:"Cartel"}]
     return (<nav>
+         <h1>Nombre del grupo</h1>
          <ul
             className="main-nav">
             {itemsMenu.map((item)=> <li><Link to = {item.ruta}> {item.name}</Link></li>)}
-           
-            </ul>
+        </ul>
     </nav>)
 }
 
