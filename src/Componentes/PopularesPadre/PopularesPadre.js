d4fec4070f971c57a8620e8f81a12e69166f771e
sofiasojoo <ssojo@udesa.edu.ar>
2025-09-10 20:16:08 -0300

    home fetch parte1

---
+++ src/Componentes/PopularesPadre/PopularesPadre.js
@@ -0,0 +1,56 @@
+import PopularesHijo from "../PopularesHijo/PopularesHijo";
+import React, {Component} from "react";
+
+class PopularesPadre extends Component{
+   constructor (props){
+    super(props);
+    this.state = {
+    data: [],
+   
+  }
+
+}
+componentDidMount() {
+  fetch("https://api.themoviedb.org/3/movie/popular?api_key=f4ad59c0cc0090a701bfacb893f62a83&language=es-ES&page=1")
+
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("datos", data);           
+      this.setState({ data: data.results }); 
+    })
+    .catch((error) => console.log("El error fue: " + error));
+}
+
+evitarSubmit(event) {
+    event.preventDefault();
+  }
+
+controlarCambios(event) {
+    this.setState({valor: event.target.value});
+  }
+
+render()
+{
+  
+  return (
+    
+  <div>
+    
+     <form onSubmit={(event)=>this.evitarSubmit(event)}>
+       <label>Name: </label>
+       <input type="text" onChange={(event)=>this.controlarCambios(event)} value={this.state.valor} />
+       <input type="submit" value="Submit" />
+     </form>
+    <section className="character-card">
+        {this.state.data === "" ? 
+        <h3>Cargando...</h3> : 
+        <h3 className="character-card">{this.state.data.map((item, idx)=> <PopularesHijo key={item + idx} info= {item} borrar = {() => this.borrar(item.id)}/>)}
+        </h3>}
+    </section> 
+    </div>
+     
+    )}}
+
+
+
+export default  PopularesPadre
--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Footer from "../../Componentes/Footer/Footer";
 import HeaderPadre from "../../Componentes/HeaderPadre/HeaderPadre";
+import PopularesPadre from "../../Componentes/PopularesPadre/PopularesPadre";
 
 function Home(){
   return (
     <React.Fragment>
       <HeaderPadre />
+      <PopularesPadre/>
       <h1>Home page</h1>
     
       <Footer />
